1597a7cd0bc4543075a152eef5a366fd34bf9236
Junaid Hassan <57299484+hjunaid22@users.noreply.github.com>
2024-01-19 22:42:26 -0500

    fatab

--- Initial_setup.sh
+++ Initial_setup.sh
@@ -8,7 +8,18 @@ echo "alias 'll=ls -al'" >> /etc/profile
 
 ########################################## Installing Applications ################################
 sudo apt-get update -y
-sudo apt-get install net-tools vim su git curl wget apache2 ca-certificates gnupg -y 
+sudo apt-get install net-tools vim su git curl wget apache2 ca-certificates gnupg nfs-common cifs-utils -y 
+
+########################################### Fstab ####################################################
+###### setup smb share to Synology ##########
+touch /etc/smbcredentials
+echo "username=download\npassword=" >> /etc/smbcredentials
+chmod 600 /etc/smbcredentials
+sudo mkdir /backup
+sudo echo "//192.168.1.5/Open_Share /backup cifs rw,uid=1001,gid=1001,credentials=/etc/smbcredentials" >> /etc/fstab
+### add \040 for the space 
+#//192.168.1.5/Plex/Kids\040Islamic\040Cartoon /Plex cifs credentials=/etc/smbcredentials,rw,uid=1002,gid=1002
+adduser download
 ########################################### Installing Jellyfin ######################################
 curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
 
